7ea1369f7a12609f30e1f9b2f6ad9dc3cc9b2be6
James Tapping <james.tapping@gmail.com>
2020-12-17 12:21:38 +0100

    first commit

---
+++ GitTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest
+//
+//  Created by James Tapping on 17/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
